7b4aef4393fd358823a4010a63ed7e3b0438cb52
Omega <78522158+Omega493@users.noreply.github.com>
2025-06-25 13:22:30 +0530

    Update complex_numbers.cpp

--- classes_and_objects/complex_numbers.cpp
+++ classes_and_objects/complex_numbers.cpp
@@ -12,10 +12,19 @@ private:
 	T imag;
 
 public:
+
+	static const Complex<T> Zero;
 	
 	Complex(T x = 0.0, T y = 0.0) { // Initialize complex numbers to 0 if no arguments are passed.
 		real = x;
 		imag = y;
+	}
+
+	bool operator== (const Complex& Z) const {
+		return (real == Z.real && imag == Z.imag);
+	}
+	bool operator!= (const Complex& Z) const {
+		return !(*this == Z);
 	}
 
 	Complex<T> operator+ (const Complex& Z) const {
@@ -26,6 +35,23 @@ public:
 		return Complex(real - Z.real, imag - Z.imag);
 	}
 
+	Complex<T> operator* (const Complex& Z) const {
+		
+		// (a + ib) * (c + id) = (ac - bd) + i (ad + bc)
+		
+		return Complex((real * Z.real) - (imag * Z.imag), (real * Z.imag) + (imag * Z.real));
+	}
+
+	Complex<T> operator/ (const Complex& Z) const {
+		
+		// (a + ib) / (c + id) = (ac + bd) + i (bc - ad)
+		// TODO: Add checking for Z = 0.
+		
+		const T prod = Z.real * Z.real + Z.imag * Z.imag;
+
+		return Complex(((real * Z.real) + (imag * Z.imag)) / prod, ((imag * Z.real) - (real * Z.imag)) / prod);
+	}
+
 	void display() const {
 		cout << "(" << real << ", " << imag << ")" << endl;
 	}
@@ -33,16 +59,24 @@ public:
 
 int main() {
 	
-	Complex<double> a(2.732, 3.1415), b(1.616, 0.693), c, d;
+	Complex<double> a(2.732, 3.1415), b(1.616, 0.693), c, d, e, f;
 	
 	c = a + b;
 	d = a - b;
+	e = a * b;
+	f = a / b;
 
 	cout << "C = ";
 	c.display();
 
 	cout << "d = ";
 	d.display();
+
+	cout << "e = ";
+	e.display();
+
+	cout << "F = ";
+	f.display();
 
 	return 0;
 }
